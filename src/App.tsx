ed6589db315b4e78c49997759c1d2d002d0538a1
marcelo-rodriguez-dev <marcelo.rodriguez@trio.dev>
2022-06-08 15:47:05 -0400

    UseEffect + UseRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { Counter } from "./components/Counter";
 import { Usuario } from "./components/Usuario";
 
+import { TimerPadre } from "./components/TimerPadre";
+
 function App() {
   return (
     <>
       <h1>React + TypeScript</h1>
       <hr />
+      <h2>UseState</h2>
       <Counter />
       <Usuario />
+      <h2>UseEffect + useRef</h2>
+      <TimerPadre />
     </>
   );
 }
